46a5ff1b3d25afb34f6c0922a4f8aa6877dca8ed
ybqdren <withzhaowen@126.com>
2021-09-06 22:00:03 +0800

    update

---
+++ src/www/github/ybqdren/VowelCount_2.java
@@ -0,0 +1,37 @@
+package www.github.ybqdren;
+
+/**
+ * Wen(Joan) Zhao <withzhaowen@126.com>
+ * 2021/9/6
+ */
+public class VowelCount_2 {
+
+    public static void main(String[] args) {
+        int count = getCount("I love you. a e i o u");
+        System.out.println(count);
+    }
+
+
+    public static int getCount(String str){
+        int vowelsCount = 0;
+
+        // a，e，i，o，u
+        char[] letters = str.toCharArray();
+        for(int i=0 ; i<letters.length ; i++){
+            if(32 == letters[i]){ continue; }
+            if(isMatch(letters[i])){ vowelsCount += 1; }
+        }
+
+        return vowelsCount;
+    }
+
+    public static boolean isMatch(char c){
+        char[] chars = {'a','e','i','o','u'};
+
+        for(int i=0; i<chars.length ; i++){
+            if(chars[i] == c){ return true; }
+        }
+
+        return false;
+    }
+}
